8d3b5b972c726e926f9e34ca796a9da963f014c5
HoJun Yang <didghwns0514@gmail.com>
2023-12-14 21:33:44 +0900

    setting.py 수정 & 앱추가

---
+++ app/apps.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class AppConfig(AppConfig):
+    """app config"""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "app"
---
+++ catube/apps.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class CatubeConfig(AppConfig):
+    """catube config"""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "catube"
